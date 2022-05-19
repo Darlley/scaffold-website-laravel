d8d1c499352b97a9b76e27660a754a4050560a2c
Darlley <systembrtt@gmail.com>
2022-05-19 10:37:22 -0400

    install livewire

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,8 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+
+        <livewire:styles />
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
@@ -128,5 +130,7 @@
                 </div>
             </div>
         </div>
+
+        <livewire:scripts />
     </body>
 </html>
